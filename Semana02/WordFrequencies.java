b42a16649167c286de99ef9a2365604912765a17
Paulo Freitas <paulo.freitas@ideais.com.br>
2017-01-05 23:21:37 -0200

    Capitulo 01 Segunda Semana

---
+++ Semana02/WordFrequencies.java
@@ -0,0 +1,11 @@
+
+/**
+ * Escreva a descrição da classe WordFrequencies aqui.
+ * 
+ * @author (seu nome) 
+ * @version (número de versão ou data)
+ */
+import edu.duke.*;
+public class WordFrequencies{
+    
+}
