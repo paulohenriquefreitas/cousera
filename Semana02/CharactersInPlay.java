bfeaf6b94a1ace74fbbfa2b45a3b47b487330ec6
Paulo Freitas <paulo.freitas@ideais.com.br>
2017-01-09 22:57:33 -0200

    Semana 02 Assignment 2: Character Names

---
+++ Semana02/CharactersInPlay.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class CharactersInPlay here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import edu.duke.*;
+import java.util.ArrayList;
+public class CharactersInPlay{
+    private ArrayList<String> names;
+    private ArrayList<Integer> counts;
+    
+    public CharactersInPlay(){
+        names = new ArrayList<String>();
+        counts = new ArrayList<Integer>();
+    }
+    
+    public void update (String name){
+    }
+    
+    public void findAllCharacters(){
+        
+    }
+}
--- Semana02/WordFrequencies.java
+++ Semana02/WordFrequencies.java
@@ -1,11 +1,53 @@
 
-/**
- * Escreva a descrição da classe WordFrequencies aqui.
- * 
- * @author (seu nome) 
- * @version (número de versão ou data)
- */
 import edu.duke.*;
+import java.util.ArrayList;     
 public class WordFrequencies{
+   
+    private ArrayList<String> myWords;
+    private ArrayList<Integer> myFreqs;
     
+    public WordFrequencies(){
+        myWords = new ArrayList<String>();
+        myFreqs = new ArrayList<Integer>();
+    }
+    
+    
+    public void findUnique(){
+        FileResource resource = new FileResource();
+        myWords.clear();
+        myFreqs .clear();
+        for(String s : resource.words()){
+            s= s.toLowerCase();
+            int index = myWords.indexOf(s);
+            if (index == -1){
+                myWords.add(s);
+                myFreqs.add(1);
+            }else{
+                myFreqs.set(index,myFreqs.get(index) + 1);
+            }
+        }
+    }
+    
+    
+    public void tester(){
+        findUnique();
+        System.out.println("Number of unique words: " + myWords.size());
+        for(int k=0; k < myWords.size(); k++){
+            System.out.println(myFreqs.get(k) + "\t" + myWords.get(k));
+        }
+        int maxIndex= findIndexOfMax();
+        System.out.println("The word that occurs most often and its count are: " + myFreqs.get(maxIndex) + "\t" + myWords.get(maxIndex)); ;
+    }
+    
+    public int findIndexOfMax(){
+        int max = 0;
+        for(Integer i: myFreqs){
+            if(max < i){
+                max = i;
+            }
+        }
+        return myFreqs.indexOf(max);
+    }   
+    
+            
 }
