ae22648a64d70d677717393f00562bbc4958257e
Paulo <paulo.freitas@b2wdigital.com>
2017-01-25 11:06:36 -0200

    CodonCount

---
+++ Semana02/CodonCount.java
@@ -0,0 +1,28 @@
+
+/**
+ * Escreva a descrição da classe CodonCount aqui.
+ * 
+ * @author (seu nome) 
+ * @version (número de versão ou data)
+ */
+import java.util.*;
+public class CodonCount {
+    
+    private HashMap<String,Integer> dnaMap;
+    
+    public CodonCount(){
+        dnaMap = new HashMap();
+    }
+    
+    public void buildCodonMap(Integer start, String dna){
+        HashMap<String,Integer> codomMap = new HashMap<String,Integer>();
+        
+    }
+    
+    public String getMostCommonCodon(){
+    }
+    
+    public printCodonCounts(int start, int end){
+    }
+
+}
